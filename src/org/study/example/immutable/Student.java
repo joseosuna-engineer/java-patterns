6affd5ed722db0e0f149db8e84486c80467ab4a9
José Osuna <joseosuna.engineer@gmail.com>
2015-10-02 13:09:36 -0300

    Create Student.java

---
+++ src/org/study/example/immutable/Student.java
@@ -0,0 +1,28 @@
+
+
+package org.study.example.immutable;
+
+
+
+public class Student {
+
+    private int id;
+    private String name;
+
+    private Student() {
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
