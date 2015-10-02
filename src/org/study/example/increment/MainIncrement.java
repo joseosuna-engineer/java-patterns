ca22b71c0fdab9558f3b9846c14631fc2560137e
José Osuna <joseosuna.engineer@gmail.com>
2015-10-02 12:48:51 -0300

    Create MainIncrement.java

---
+++ src/org/study/example/increment/MainIncrement.java
@@ -0,0 +1,14 @@
+
+
+package org.study.example.increment;
+
+
+
+public class MainIncrement {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i++);
+        System.out.println(++i);
+    }
+  
+}
