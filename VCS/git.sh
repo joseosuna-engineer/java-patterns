bce7b071c67dd4af8ff81f5c0cee0a498fbb4a4d
José Osuna <joseosuna.engineer@gmail.com>
2015-10-07 05:57:45 -0400

    Update git.sh

--- VCS/git.sh
+++ VCS/git.sh
@@ -1,5 +1,13 @@
+/*
+ * Always put .ssh on user directory and .gitconfig file too 
+ */
+ 
 /* list all config variables */
 git config --list 
+
 /* Your Identity */
 git config --global user.name "John Doe"
 git config --global user.email johndoe@example.com
+
+/* clone */
+git clone user@server:path/to/repo.git destination/path
