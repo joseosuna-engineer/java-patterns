0c2b0a467aca5fad59653a1fa04ca071a929adaf
Jose Osuna <joseosuna.engineer@gmail.com>
2015-10-08 07:49:52 -0400

    Update main class client Singleton Pattern.

--- JavaApplication/src/org/study/example/pattern/singleton/MainSingleton.java
+++ JavaApplication/src/org/study/example/pattern/singleton/MainSingleton.java
@@ -3,7 +3,16 @@ package org.study.example.pattern.singleton;
 public class MainSingleton {
 
     public static void main(String[] args) {
+        try {
+            Universe godUniverse = Universe.getUniverse();
+            Universe bigBangUniverse = Universe.getUniverse();
 
+            godUniverse.setGalaxyCount(3);
+
+            System.out.println("Galaxies in God Universe: " + godUniverse.getGalaxyCount());
+            System.out.println("Galaxies in Big Bang Universe: " + bigBangUniverse.getGalaxyCount());
+        } catch (Exception e) {
+        }
     }
 
 }
