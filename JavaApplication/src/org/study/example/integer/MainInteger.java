9e7332939e16f1e40a5677f7f1ce013789e91d85
Jose Osuna <joseosuna.engineer@gmail.com>
2015-10-18 13:04:42 -0300

    implementing Integer util client

--- JavaApplication/src/org/study/example/integer/MainInteger.java
+++ JavaApplication/src/org/study/example/integer/MainInteger.java
@@ -1,9 +1,15 @@
 package org.study.example.integer;
 
+import java.util.List;
+
 public class MainInteger {
 
     public static void main(String[] args) {
-
+        int[] array = {1, 2, 3, 7, 7, 6, 1};
+        List repeted = IntegerUtil.repeted(array);
+        for (Object element : repeted) {
+            System.out.println(element);
+        }
     }
 
 }
